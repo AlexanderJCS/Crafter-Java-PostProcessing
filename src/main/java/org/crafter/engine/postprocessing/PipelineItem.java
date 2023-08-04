16ad67fbb78eb0d37abfb69962e69158bcbcd02d
Alex <alexandercastronovo@gmail.com>
2023-08-04 00:33:20 -0400

    Fix PipelineItem being unbound during construction when it shouldn't

--- src/main/java/org/crafter/engine/postprocessing/PipelineItem.java
+++ src/main/java/org/crafter/engine/postprocessing/PipelineItem.java
@@ -69,8 +69,6 @@ public class PipelineItem implements AutoCloseable {
     }
 
     private void refreshFramebufferTexture() {
-        this.bind();
-
         glBindTexture(GL_TEXTURE_2D, this.framebufferTexture);
 
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, Window.getWindowWidth(), Window.getWindowHeight(), 0, GL_RGB, GL_UNSIGNED_BYTE, (ByteBuffer) null);
@@ -79,8 +77,6 @@ public class PipelineItem implements AutoCloseable {
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
         glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, this.framebufferTexture, 0);
-
-        this.unbind();
     }
 
     private static void errorCheck() {
